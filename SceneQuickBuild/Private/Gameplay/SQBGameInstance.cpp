69b8d947739460319571e1ce3e021cc7ec5326cc
haiaimi <wanghai1997@outlook.com>
2018-07-30 18:17:00 +0800

    添加TGameInstance用来管理场景中的BaseActor

---
+++ SceneQuickBuild/Private/Gameplay/SQBGameInstance.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SQBGameInstance.h"
+
+
+
+
+USQBGameInstance::USQBGameInstance()
+{
+
+}
+
+void USQBGameInstance::RegisterBaseActor(class ABaseActor* InRef)
+{
+
+}
---
+++ SceneQuickBuild/Public/Gameplay/SQBGameInstance.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SQBGameInstance.generated.h"
+
+/**
+ *  游戏Instance类，用于BaseActor之间的通信,是通信的中间类
+ */
+UCLASS()
+class SCENEQUICKBUILD_API USQBGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	USQBGameInstance();
+	
+public:
+	/** 注册BaseActor，以便相互之间的通信*/
+	void RegisterBaseActor(class ABaseActor* InRef);
+
+public:
+	TArray<class ABaseActor> AllBaseActor;
+};
